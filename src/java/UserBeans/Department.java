057dd5134f649673cfa75c528424e50dcb085e54
Krzysztof Huszcza <kwh109@ic.ac.uk>
2011-06-04 15:31:32 +0100

    Added Deletion of Department Button

--- src/java/UserBeans/Department.java
+++ src/java/UserBeans/Department.java
@@ -5,6 +5,12 @@
 
 package UserBeans;
 
+import Connections.ConnectionSingleton;
+import java.io.IOException;
+import java.sql.Statement;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
 
 /**
  *
@@ -16,9 +22,28 @@ public class Department {
     public Department() {
     }
 
-    public void deleteDepartment() {
+    public void deleteDepartment() throws IOException {
 
+        FacesContext facesContext
+                = (FacesContext) FacesContext.getCurrentInstance();
+        HttpSession session
+                = (HttpSession) facesContext.getExternalContext()
+                    .getSession(false);
+
+        String username
+                = (String) session.getAttribute(ConnectionSingleton.idname);
+        try {
+            Statement statement
+              = Connections.ConnectionSingleton.createConnection().getStatement();
+            statement.execute
+              ("DELETE FROM PlayerResources WHERE IdName = '" + username + "'");
+            statement.execute
+              ("DELETE FROM DepartmentInfo WHERE IdName = '" + username + "'");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
+
 
     public String startPlaying() {
 

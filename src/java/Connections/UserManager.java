47cf8815494c319d012e7cbe4ff6eecb43ca977c
Karol Pysniak <kp1209@ic.ac.uk>
2011-06-11 14:04:42 +0100

    Playing grid panel added

---
+++ src/java/Connections/UserManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Connections;
+
+/**
+ *
+ * @author kp1209
+ */
+public class UserManager {
+
+    /* Singleton */
+    private UserManager() {
+
+    }
+
+}

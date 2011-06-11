aadfc65c3e76508ac2236940dfa43e7b83df61ea
Krzysztof Huszcza <kwh109@ic.ac.uk>
2011-06-11 12:43:16 +0100

    Basic event classes

---
+++ src/java/events/BarNight.java
@@ -0,0 +1,15 @@
+package events;
+
+public class BarNight extends Event {
+
+    @Override
+    public void invoke() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getInfo() {
+        return "BarNight";
+    }
+
+}

f2288a555107276e3e8a73f07b8bb2cfc9c28ba3
Karol Pysniak <kp1209@ic.ac.uk>
2011-06-12 17:51:57 +0100

    Trading started

---
+++ src/java/messageSystem/TradeOffer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package messageSystem;
+
+/**
+ *
+ * @author kp1209
+ */
+public class TradeOffer {
+
+    private String receiverId;
+    private String giverId;
+
+    private ResourcesType resourcesType;
+    private int amount;
+
+
+
+}

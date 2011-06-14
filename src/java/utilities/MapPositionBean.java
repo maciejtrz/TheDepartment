45295c71e63f1a825939eb5368c22c2b6733c203
Krzysztof Huszcza <kwh109@ic.ac.uk>
2011-06-14 20:27:10 +0100

    Map backed bean added.

---
+++ src/java/utilities/MapPositionBean.java
@@ -0,0 +1,117 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utilities;
+
+
+/**
+ *
+ * @author kwh109
+ */
+public class MapPositionBean {
+
+
+    private int position;
+
+    /** Creates a new instance of MapPositionBean */
+    public MapPositionBean() {
+        position = 0;
+    }
+
+    public int getPosition1() {
+        position = 1;
+        return position;
+    }
+
+    public int getPosition2() {
+        position = 2;
+        return position;
+    }
+
+
+    public int getPosition3() {
+        position = 3;
+        return position;
+    }
+
+
+    public int getPosition4() {
+        position = 4;
+        return position;
+    }
+
+
+    public int getPosition5() {
+        position = 5;
+        return position;
+    }
+
+
+    public int getPosition6() {
+        position = 6;
+        return position;
+    }
+
+
+    public int getPosition7() {
+        position = 7;
+        return position;
+    }
+
+
+    public int getPosition8() {
+        position = 8;
+        return position;
+    }
+
+
+    public int getPosition9() {
+        position = 9;
+        return position;
+    }
+
+
+    public int getPosition10() {
+        position = 10;
+        return position;
+    }
+
+
+    public int getPosition11() {
+        position = 111;
+        return position;
+    }
+
+
+    public int getPosition12() {
+        position = 12;
+        return position;
+    }
+
+
+    public int getPosition13() {
+        position = 13;
+        return position;
+    }
+
+
+    public int getPosition14() {
+        position = 14;
+        return position;
+    }
+
+
+    public int getPosition15() {
+        position = 15;
+        return position;
+    }
+
+
+    public int getPosition16() {
+        position = 16;
+        return position;
+    }
+
+}

507fd764fc9ba43ba3c1198824caae358bb1d483
Piotr Kraus <pk2109@ic.ac.uk>
2011-06-17 19:44:20 +0100

    =prof shop outlook done. Lack of proper functionality  ( buy method needs to be fixed so that it makes changes to db

--- src/java/UserBeans/BuyLecturers.java
+++ src/java/UserBeans/BuyLecturers.java
@@ -1,7 +1,14 @@
 package UserBeans;
 
+import ConnectionDataBase.CapacityHelper;
+import ConnectionDataBase.Playerresources;
+import Connections.ConnectionSingleton;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import utilities.BasicUtils;
 import utilities.Lecturer;
 import utilities.LecturerBenefits;
 import utilities.LecturersManager;
@@ -39,11 +46,39 @@ public class BuyLecturers implements Serializable  {
         return selected_lecturer.getSpecializations();
     }
 
-    public void buy() {
-        LecturersManager mgr 
+    public String buy() {
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+        Auth auth = (Auth) session.getAttribute(ConnectionSingleton.Auth);
+
+        LecturersManager mgr
                 = new LecturersManager(utilities.BasicUtils.getUserName());
+
+
+        Playerresources resources = auth.getResources();
+        CapacityHelper capacityhelper = new CapacityHelper();
+        if( capacityhelper.getCapacity(auth.getRemember()).getProfessorscapacity() <=  mgr.getOwnedLecturers().size() ){
+
+                FacesContext.getCurrentInstance().addMessage(
+                BasicUtils.findComponent(facesContext.getViewRoot(),"submitProf").getClientId(facesContext),
+                new FacesMessage(FacesMessage.SEVERITY_ERROR," Me ", "Not enought space for new Prof!!!"));
+                return "fail";
+
+        } else if ( resources.getMoney() < selected_lecturer.getPrice()){
+
+             FacesContext.getCurrentInstance().addMessage(
+             BasicUtils.findComponent(facesContext.getViewRoot(),"submitProf").getClientId(facesContext),
+             new FacesMessage(FacesMessage.SEVERITY_ERROR," Me ", "This guy is too expensive for your DoC!!!"));
+             return "fail";
+        }
+
         mgr.purchaseLecturer(selected_lecturer.getName());
+
+        return "success";
     }
+
+
 
 
 }

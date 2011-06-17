c7c9678952e6eb822d97f733a7b68818a7f161d8
Karol Pysniak <kp1209>
2011-06-17 12:35:25 +0100

    Error checking added in notice offers creation

--- src/java/messageSystem/NoticeMessageWriter.java
+++ src/java/messageSystem/NoticeMessageWriter.java
@@ -6,6 +6,9 @@
 package messageSystem;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import utilities.BasicUtils;
 
 
@@ -19,9 +22,67 @@ public class NoticeMessageWriter extends TradeWriter implements Serializable  {
         super(MessageSingleton.NOTICE_BOARD_OFFER);
     }
 
-    public void sendNoticeOffer() {
+    public String sendNoticeOffer() {
         getTradeOffer().setSenderid(BasicUtils.getUserName());
-        OffersSuperviser.getNoticeMonitor().addNoticeOffer(getTradeOffer());
+
+        String result = null;
+        if(validate()) {
+            OffersSuperviser.getNoticeMonitor().addNoticeOffer(getTradeOffer());
+            result = "success";
+        }
+
+        return result;
+    }
+
+    private boolean validate() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        
+        boolean error = true;
+
+        if (getExpireDate().before(new Date())) {
+            System.out.println("Adding new message for expire date...");
+            FacesContext.getCurrentInstance().addMessage(
+                    BasicUtils.findComponent(facesContext.getViewRoot(),
+                    "expireNoticeOfferDate").getClientId(facesContext),
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Expire date error",
+                    "Expire date of trade offer cannot be earlier than the current time!"));
+
+            error = false;
+        }
+
+        if(getAmountWanted() <= 0) {
+            FacesContext.getCurrentInstance().addMessage(
+                    BasicUtils.findComponent(facesContext.getViewRoot(),
+                    "wantedNoticeOfferAmount").getClientId(facesContext),
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Wanted Amount Limi",
+                    "Wanted amount must be greater than 0"));
+
+            error = false;
+        }
+
+        if(getAmountOffered() <= 0) {
+            FacesContext.getCurrentInstance().addMessage(
+                    BasicUtils.findComponent(facesContext.getViewRoot(),
+                    "offeredNoticeOfferAmount").getClientId(facesContext),
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Offered Amount Limi",
+                    "Offered amount must be greater than 0"));
+
+            error = false;
+        }
+
+        if(getResourcesOfferedType() == getResourcesWantedType()) {
+            FacesContext.getCurrentInstance().addMessage(
+                    BasicUtils.findComponent(facesContext.getViewRoot(),
+                    "sendNoticeOfferTrade").getClientId(facesContext),
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Resources type",
+                    "Reasources wanted and offered cannot be of the same type"));
+
+            System.out.println("Resources are the same...");
+
+            error = false;
+        }
+
+        return error;
     }
 
 }

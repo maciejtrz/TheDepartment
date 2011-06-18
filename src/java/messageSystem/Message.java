b3407133daa17f7d26d5be5a69fa200a67f4926e
Karol Pysniak <kp1209>
2011-06-18 01:06:21 +0100

    trades.xhtml splitted into 3 files

--- src/java/messageSystem/Message.java
+++ src/java/messageSystem/Message.java
@@ -10,17 +10,30 @@ import java.util.Iterator;
 import java.util.List;
 import javax.faces.model.SelectItem;
 
+public class Message extends Messagesystem implements Serializable {
 
-public class Message extends Messagesystem implements Serializable  {
-    
-        /*Selected reicever*/
+    /*Selected reicever*/
     private int selectedReceiver;
 
     /*List of users*/
     public List<Players> users = new ArrayList<Players>();
+    public List<SelectItem> receiverList = new ArrayList<SelectItem>();
 
     public Message() {
         setSenderid(utilities.BasicUtils.getUserName());
+        int i = 0;
+
+        PlayerHelper ph = new PlayerHelper();
+        Iterator<Players> iterator = ph.getPlayers().iterator();
+
+        while (iterator.hasNext()) {
+            Players player = iterator.next();
+
+            if (!player.getIdname().equals(getSenderid())) {
+                receiverList.add(new SelectItem(new Integer(i++), player.getIdname()));
+                users.add(player);
+            }
+        }
     }
 
     public void setSelectedReceiver(int s) {
@@ -32,29 +45,15 @@ public class Message extends Messagesystem implements Serializable  {
     }
 
     public List<SelectItem> getReceivers() {
-        List<SelectItem> receiverList = new ArrayList<SelectItem>();
-        int i = 0;
-
-        PlayerHelper ph = new PlayerHelper();
-
-        Iterator<Players> iterator = ph.getPlayers().iterator();
-
-        while (iterator.hasNext()) {
-            Players player = iterator.next();
-
-            receiverList.add(new SelectItem(new Integer(i++), player.getIdname()));
-            users.add(player);
-        }
 
         return receiverList;
     }
 
-
-        public void send() {
+    public void send() {
 
         MessageSystemHelper msghelp = new MessageSystemHelper();
         msghelp.createMessage(getSenderid(), users.get(selectedReceiver).getIdname(), getSubject(),
                 getMsg(), MessageSingleton.PLAIN_MESSAGE);
 
-        }
+    }
 }

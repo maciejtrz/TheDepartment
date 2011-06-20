ec92a142c683ebcd65ee2eeca85d1dae773ba96d
Karol Pysniak <kp1209@ic.ac.uk>
2011-06-20 16:21:22 +0100

    Research Reader fixed

--- src/java/ResearchPoints/ResearchBag.java
+++ src/java/ResearchPoints/ResearchBag.java
@@ -39,6 +39,7 @@ public class ResearchBag implements Serializable {
             researchHelper = new ResearchHelper();
             
         }
+
     }
 
     public String getUserid() {
---
+++ src/java/ResearchPoints/ResearchReader.java
@@ -0,0 +1,52 @@
+package ResearchPoints;
+
+import ConnectionDataBase.Research;
+import Connections.ConnectionSingleton;
+import java.util.List;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import org.icefaces.application.PushRenderer;
+import org.primefaces.model.TreeNode;
+import utilities.BasicUtils;
+
+
+public class ResearchReader {
+
+    private ResearchBag researchBag = null;
+
+    public ResearchReader() {
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+
+        PushRenderer.addCurrentSession(BasicUtils.getUserName() + "research");
+        researchBag = (ResearchBag) session.getAttribute(ConnectionSingleton.researchBag);
+
+    }
+
+    public List<Research> getResearches() {
+        return researchBag.getResearches();
+    }
+
+    public void setResearches(List<Research> ongoingResearches) {
+        researchBag.setResearches(ongoingResearches);
+    }
+
+    public Research getSelectedResearch() {
+        return researchBag.getSelectedResearch();
+    }
+
+    public void setSelectedResearch(Research selectedResearch) {
+
+        selectedResearch.changeState();
+    }
+
+    public TreeNode getFinishedReserchTree() {
+        return researchBag.getFinishedReserchTree();
+    }
+
+    
+
+    
+
+}

888bdd7e43b308ed3f491d12b524f1909af6890c
Krzysztof Huszcza <kwh109@ic.ac.uk>
2011-06-20 14:31:14 +0100

    Cleared from annoying prints

--- src/java/buildings/BuildingBean.java
+++ src/java/buildings/BuildingBean.java
@@ -4,8 +4,6 @@ import Connections.UserManager;
 import java.io.Serializable;
 import java.util.List;
 import javax.faces.event.ActionEvent;
-import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
 import utilities.BuildingsUtils;
 
 
@@ -36,22 +34,16 @@ public class BuildingBean implements Serializable {
     }
 
     public Building getSelectedBuilding() {
-        System.out.println("Building getter is called!");
         return selected_building;
     }
 
     public void setSelectedBuilding(Building b) {
-        System.out.println("Building setter is called!");
-        System.out.println(b.getInfo());
         selected_building = b;
     }
 
     public void buy () {
         Building s = selected_building;
         int position = UserManager.getBuilidngPosition(playerName);
-        System.out.println("Position: " + position);
-        System.out.println("Building: " + s.getInfo());
-        System.out.println("Player name: " + playerName);
         s.build(playerName, position);
         buildingList = utils.getAvailableBuildings(playerName);
 
@@ -62,7 +54,6 @@ public class BuildingBean implements Serializable {
     }
 
     public void loadCurrentBuilding(ActionEvent event) {
-        System.out.println("SELECT CALLED");
         selected_building = (Building)event.getComponent().getAttributes().get("input_building");
     }
 
